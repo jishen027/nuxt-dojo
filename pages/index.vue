a1decb32b6f58214606a4f1157980449bbab9df9
J Li <jli306@sheffield.ac.uk>
2022-11-12 01:18:36 +0000

    init project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Hello, ninja</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iusto ipsum
+      et sapiente error a voluptate ipsam velit non blanditiis?
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ProductDetails :product="product"></ProductDetails>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+
+// fetch the product
+const url = 'https://fakestoreapi.com/products/'+id
+const { data: product }  =  await useFetch(url,{key: id});
+
+definePageMeta({
+  layout: 'products'
+})
+
+</script>
+
+<style>
+
+</style>
