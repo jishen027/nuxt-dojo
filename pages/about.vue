163e1f2c813ec4542978b03e2faf01d1bb6bbe97
J Li <jli306@sheffield.ac.uk>
2022-11-23 21:28:05 +0000

    feat: add server side handler

--- pages/about.vue
+++ pages/about.vue
@@ -5,10 +5,12 @@
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iusto ipsum
       et sapiente error a voluptate ipsam velit non blanditiis?
     </p>
+    <p>{{ data }}</p>
   </div>
 </template>
 
-<script>
+<script setup>
+const { data } = await useFetch("/api/currency/GBP");
 </script>
 
 <style scoped>
